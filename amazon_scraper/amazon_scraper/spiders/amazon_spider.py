82dd324fdb45512e52081dd409f0477d281a419b
gdperalta <godfreydjperalta@gmail.com>
2022-07-15 19:06:36 +0800

    Add parse_product_response function to handle data in product page Add parse_seller_page to handle data in seller page. For debugging Remove parse_product_page function

--- amazon_scraper/amazon_scraper/spiders/amazon_spider.py
+++ amazon_scraper/amazon_scraper/spiders/amazon_spider.py
@@ -20,23 +20,74 @@ class AmazonSpider(scrapy.Spider):
             # asin = product.xpath('@data-asin').extract_first()
             asin = product.get()
             product_url = f"{base_url}/dp/{asin}"
-            yield scrapy.Request(url=self.get_url(product_url), callback=self.parse_product_page, meta={'asin': asin})
+            yield scrapy.Request(url=self.get_url(product_url), callback=self.parse_product_response, meta={'asin': asin})
         # To scrape all pages
-        next_page = response.xpath('//li[@class="a-last"]/a/@href').extract_first()
-        if next_page:
-            url = base_url + next_page
-            yield scrapy.Request(url=self.get_url(url), callback=self.parse_keyword_response)        
+        # next_page = response.xpath('//li[@class="a-last"]/a/@href').extract_first()
+        # if next_page:
+        #     url = base_url + next_page
+        #     yield scrapy.Request(url=self.get_url(url), callback=self.parse_keyword_response)        
 
 
-    def parse_product_page(self, response):
-        seller_url = response.xpath('//*[@id="bylineInfo"]/@href').extract_first()
-        seller_name = response.xpath('//*[@id="bylineInfo"]/text()').extract_first()
+    def parse_product_response(self, response):
+        seller_name = response.xpath('//*[@id="sellerProfileTriggerId"]/text()').extract_first()
         asin = response.meta['asin']
-        title = response.xpath('//*[@id="productTitle"]/text()').extract_first()
         seller_rank = response.xpath('//*[text()[contains(., "Best Sellers Rank")]]/parent::*//text()').extract()
-        rating = response.xpath('//*[@id="acrPopover"]/@title').extract_first()
-        number_of_reviews = response.xpath('//*[@id="acrCustomerReviewText"]/text()').extract_first()
-        yield {'SellerUrl': seller_url, 'SellerName': seller_name, 'asin': asin, 'Title': title, 'Rating': rating, 'NumberOfReviews': number_of_reviews, 'SellerRank': seller_rank}
+        fba = response.xpath('//*[@class="tabular-buybox-text" and @tabular-attribute-name="Ships from"]/div/span/text()').extract_first()
+
+        seller_id = response.xpath('//*[@id="merchantID"]/@value').extract_first()
+        if seller_id:
+            seller_url = f"{base_url}/sp?seller={seller_id}"
+            yield scrapy.Request(url=self.get_url(seller_url),
+                                callback=self.parse_seller_page,
+                                meta={
+                                        'SellerUrl': seller_url,
+                                        'SellerName': seller_name,
+                                        'asin': asin,
+                                        'SellerRank': seller_rank,
+                                        'FBA': fba
+                                    })
+        else:
+            return
+
+
+
+    def parse_seller_page(self, response):
+        seller_url = response.meta['SellerUrl']
+        seller_name = response.meta['SellerName']
+        asin = response.meta['asin']
+        seller_rank = response.meta['SellerRank']
+        fba = response.meta['FBA']
+        rating = response.xpath('//*[@id="feedback-summary-table"]/tbody/tr[2]/td[5]/span/text()').get()
+        number_of_reviews = response.xpath('//*[@id="feedback-summary-table"]/tbody/tr[5]/td[5]/span/text()').get()
+        
+        x = response.xpath('//*[@id="feedback-summary-table"]/tbody/tr[2]/td[5]/span/text()')
+        y = response.xpath('//*[@id="feedback-summary-table"]/tbody/tr[5]/td[5]/span/text()')
+
+        print(response)
+        print(x)
+        print(y)
+        print('hello')
+
+        yield {
+                'SellerUrl': seller_url,
+                'SellerName': seller_name,
+                'FBA': fba,
+                'ASIN': asin,
+                'Rating': rating,
+                'NumberOfReviews': number_of_reviews,
+                'SellerRank': seller_rank,
+              }
+
+
+    # def parse_product_page(self, response):
+    #     seller_url = response.xpath('//*[@id="bylineInfo"]/@href').extract_first()
+    #     seller_name = response.xpath('//*[@id="bylineInfo"]/text()').extract_first()
+    #     asin = response.meta['asin']
+    #     title = response.xpath('//*[@id="productTitle"]/text()').extract_first()
+    #     seller_rank = response.xpath('//*[text()[contains(., "Best Sellers Rank")]]/parent::*//text()').extract()
+    #     rating = response.xpath('//*[@id="acrPopover"]/@title').extract_first()
+    #     number_of_reviews = response.xpath('//*[@id="acrCustomerReviewText"]/text()').extract_first()
+    #     yield {'SellerUrl': seller_url, 'SellerName': seller_name, 'asin': asin, 'Title': title, 'Rating': rating, 'NumberOfReviews': number_of_reviews, 'SellerRank': seller_rank}
 
     
     def get_url(self, url):
